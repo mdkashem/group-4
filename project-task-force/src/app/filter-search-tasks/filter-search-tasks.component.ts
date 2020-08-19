d443147a1c543f42bbf52e25c786ad229a901775
unknown <lbierer@gmail.com>
2020-08-19 14:50:40 -0500

    added navbar and corresponding components.  Installed bootstrap

---
+++ project-task-force/src/app/filter-search-tasks/filter-search-tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-search-tasks',
+  templateUrl: './filter-search-tasks.component.html',
+  styleUrls: ['./filter-search-tasks.component.css']
+})
+export class FilterSearchTasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ project-task-force/src/app/select-view-task/select-view-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-view-task',
+  templateUrl: './select-view-task.component.html',
+  styleUrls: ['./select-view-task.component.css']
+})
+export class SelectViewTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
