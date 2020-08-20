977b151eb79ec5a5ad1fdaf04b54d2763f0447f2
TR-1000 <t.l.ross@outlook.com>
2020-08-20 11:10:06 -0500

    added components and service for user registration and login

--- project-task-force/src/app/app.module.ts
+++ project-task-force/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AuthorizationModule } from './authorization/authorization.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthorizationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
