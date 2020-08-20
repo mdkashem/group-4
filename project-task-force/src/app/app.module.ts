ff4f558baa15f0fe878750d1ed1a74c114c99a2f
Md Abul Kashem <56566230+mdkashem@users.noreply.github.com>
2020-08-20 15:01:10 -0700

    I can display task from api

--- project-task-force/src/app/app.module.ts
+++ project-task-force/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayTaskComponent } from './display-task/display-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayTaskComponent
   ],
   imports: [
     BrowserModule,
