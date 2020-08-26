fae0074bef21f1c3318994bcb5aac90068a24b50
Md Abul Kashem <56566230+mdkashem@users.noreply.github.com>
2020-08-26 13:05:06 -0700

    Refactor search/filter

--- project-task-force/src/app/filter-search-tasks/filter-search-tasks.component.ts
+++ project-task-force/src/app/filter-search-tasks/filter-search-tasks.component.ts
@@ -10,50 +10,7 @@ export class FilterSearchTasksComponent implements OnInit {
   title = 'custom-search-filter-example';
   searchedKeyword: string;
 
-  filterResultDataSet = [
-    {
-      firstName: 'Cristiano',
-      lastName: 'Ronaldo',
-      country: 'Pourtgal',
-      club:'Juventus'
-    },
-    {
-      firstName: 'Leo',
-      lastName: 'Messi',
-      country: 'Argentina',
-      club:'Barcelona'
-    },
-    {
-      firstName: 'Neymar',
-      lastName: 'Junior',
-      country: 'BRAZIL',
-      club:'PSG'
-    },
-    {
-      firstName: 'Sergio',
-      lastName: 'Ramos',
-      country: 'SPAIN',
-      club:'Real Madrid'
-    },
-    {
-      firstName: 'Karim',
-      lastName: 'Benzema',
-      country: 'France',
-      club:'Real Madrid'
-    },
-    {
-      firstName: 'Sergio',
-      lastName: 'Buiscut',
-      country: 'SPAIN',
-      club:'Barcelona'
-    },
-    {
-      firstName: 'Gerard',
-      lastName: 'Pique',
-      country: 'SPAIN',
-      club:'Barcelona'
-    }
-  ]
+  
 
   tasks: Task[];  
   constructor( private taskService: ToDoServiceService) { }
@@ -63,6 +20,23 @@ export class FilterSearchTasksComponent implements OnInit {
   }
   getTasks(): void {
     this.taskService.getTasks().subscribe(tasks=>this.tasks = tasks)
+  }
+
+  selectedTask: Task;
+  onSelect(task: Task): void {
+    this.selectedTask = task;
+  }
+  updateTask($event): void {
+
+    console.log("update button was clicked! " , $event);
+    this.taskService.updateTask(this.selectedTask)
+    .subscribe();
+  }
+
+  delete(task: Task): void {
+    this.tasks = this.tasks.filter(h => h !== task);
+    //this.heroService.deleteHero(hero).subscribe();
+    this.taskService.deleteTask(task.id).subscribe();
   }
 
 }
